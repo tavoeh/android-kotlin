8352ec1662efbae8342170b24ed4bcd6b08798a8
Gustavo Enriquez <tavoeh@gmail.com>
2017-02-08 00:59:02 +1100

    Adding RecycleView dependency. Creating RecycleView, adapter and list to show a static weather list

--- app/src/main/java/com/tavoeh/kotlin/MainActivity.kt
+++ app/src/main/java/com/tavoeh/kotlin/MainActivity.kt
@@ -2,15 +2,33 @@ package com.tavoeh.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.find
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        hello.text = "Hello from Kotlin!"
+        val forecastList : RecyclerView =  find(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
     }
+
+
+
 }
